c64866415c4caf05ac8a9fbc576d1203b7b559b0
Manpreet <35901582+m81092@users.noreply.github.com>
2018-10-28 17:38:09 -0400

    Comments added
    Service package

--- Part-1/src/org/brewingjava/service/ProductServiceImpl.java
+++ Part-1/src/org/brewingjava/service/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package org.brewingjava.service;
 
-import java.awt.Event;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,28 +9,44 @@ import org.brewingjava.dao.EventHandlerDAO;
 import org.brewingjava.dao.EventHandlerDAOImpl;
 import org.brewingjava.model.Books;
 
+/*
+ * This class implements the methods from ProductService DAO
+ */
 public class ProductServiceImpl implements ProductService {
 
+	/*
+	 * This method calls the method in DAO to get the booklist and returns an
+	 * Arraylist
+	 */
 	@Override
 	public ArrayList<Books> getAllBooks() {
-		// TODO Auto-generated method stub
+
 		ArrayList<Books> bookList = new ArrayList<Books>();
 		BookDAO dao = new BookDAOImpl();
 		bookList = dao.getAllBooks();
-
 		return bookList;
 	}
 
+	/*
+	 * This method calls the method in DAO to get the books by category and returns
+	 * the list of book type
+	 */
 	@Override
 	public List<Books> getBooksByCategory(String category) {
-		// TODO Auto-generated method stub
+
 		List<Books> bookList = new ArrayList<Books>();
 		BookDAO dao = new BookDAOImpl();
 		bookList = dao.getBooksByCategory(category);
 		return bookList;
 	}
 
+	/*
+	 * This method calls the method in DAO to get the book info and silently adds
+	 * the book event in the db
+	 */
+	@Override
 	public ArrayList<Books> getBookInfo(int id, String event) {
+
 		ArrayList<Books> bookList = new ArrayList<Books>();
 		ArrayList<Books> bookEventList = new ArrayList<Books>();
 		Books eventBook = new Books();
