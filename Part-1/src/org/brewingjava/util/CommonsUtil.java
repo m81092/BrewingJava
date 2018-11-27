cdfc5d23bb82160143be3e240ae93c1e20d5a9b9
tarun manchanda <tarunmanchanda1903@gmail.com>
2018-11-26 22:12:34 -0500

    cheap fix 2
    repliate https to http inside code

--- Part-1/src/org/brewingjava/util/CommonsUtil.java
+++ Part-1/src/org/brewingjava/util/CommonsUtil.java
@@ -6,7 +6,7 @@ public class CommonsUtil {
 	
 	public static String getBaseUrl(HttpServletRequest request) {
 		String baseUrl = request.getRequestURL().substring(0, request.getRequestURL().length() - request.getRequestURI().length()) + request.getContextPath();
-		return baseUrl.replaceAll("https", "http");
+		return baseUrl.replaceAll("https", "http").replaceAll("8443", "8080");
 	}
 
 }
